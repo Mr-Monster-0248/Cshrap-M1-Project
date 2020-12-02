4a2dc871ea681d08edc9eb3aad2d831c9368ec8b
Thibault LEPEZ <thibault.lepez@efrei.net>
2020-12-02 16:09:13 +0100

    Ajoutez des fichiers projet.

---
+++ ProjectServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
