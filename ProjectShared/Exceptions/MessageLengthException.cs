8b9ff53b4067ea2a3fbf141da2023aae4a3633fe
Thibault LEPEZ <thibault.lepez@efrei.net>
2020-12-03 13:35:00 +0100

    Add Message exception

---
+++ ProjectShared/Exceptions/MessageLengthException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectShared.Exceptions
+{
+    class MessageLengthException : Exception
+    {
+        public MessageLengthException(int eccess)
+            : base($"Message exceed 280 char limit with an overflow of {eccess}") { }
+    }
+}
