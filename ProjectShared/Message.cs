be4c6dadf4d38a3b993c0c39241e861ff119a284
Thibault LEPEZ <thibault.lepez@efrei.net>
2020-12-03 13:34:48 +0100

    Start Entity classes

---
+++ ProjectShared/Message.cs
@@ -0,0 +1,32 @@
+﻿using ProjectShared.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectShared
+{
+    class Message
+    {
+        private string _text;
+
+        public DateTime CreatedTime { get; }
+        public int Id { get; private set; }
+        public string Text 
+        { 
+            get => _text;
+            set
+            {
+                if (value.Length > 280) throw new MessageLengthException(value.Length - 280);
+                else _text = value;
+            }
+        }
+
+        public Message(int id, string text)
+        {
+            Id = id;
+            Text = text;
+            CreatedTime = DateTime.UtcNow;
+        }
+
+    }
+}
