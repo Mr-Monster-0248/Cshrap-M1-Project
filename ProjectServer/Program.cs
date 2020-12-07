80fd83ec7115392dcf5dc6c7d79863f089c7e3b6
Thibault LEPEZ <thibault.lepez@efrei.net>
2020-12-07 13:59:22 +0100

    Add logger

--- ProjectServer/Program.cs
+++ ProjectServer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Serilog;
 
 namespace ProjectServer
 {
@@ -6,7 +7,13 @@ namespace ProjectServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Server server = new Server();
+            server.Start();
         }
     }
 }
