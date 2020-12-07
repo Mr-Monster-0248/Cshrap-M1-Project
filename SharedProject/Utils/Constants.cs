132445f40bbdf3f65971e659bb829f1f205e31d7
Thibault LEPEZ <thibault.lepez@efrei.net>
2020-12-07 14:00:36 +0100

    Add Constants

---
+++ SharedProject/Utils/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace SharedProject.Utils
+{
+    public static class Constants
+    {
+        public const Int32 MaxByte = 1024;
+        public const string LocalHost = "127.0.0.1";
+        public const string Port = "1234";
+        public static readonly Regex RequestRegex = new Regex(@"^[a-z]*:{.*}$");
+    }
+}
