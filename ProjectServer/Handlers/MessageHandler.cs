97e9cd0d0142c4c1fab85d0d16de0be9bed309fc
Thibault LEPEZ <thibault.lepez@efrei.net>
2020-12-15 16:12:25 +0100

    Add some more handlers

---
+++ ProjectServer/Handlers/MessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SharedProject.CommandUtils;
+
+namespace ProjectServer.Handlers
+{
+    public partial class WebSocketHandler
+    {
+        private void HandleDirectMessage(DirectMessageData data)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HandleTopicMessage(TopicMessageData data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectServer/Services/TopicService.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using ProjectServer.Models;
+
+namespace ProjectServer.Services
+{
+    public class TopicService
+    {
+        /// <summary>
+        /// Get all the topics available
+        /// </summary>
+        /// <returns>A list of Topic object</returns>
+        public static List<Topic> GetTopics()
+        {
+            using var context = DbServices.Instance.Context;
+            return context.Topics.ToList();
+            
+        }
+
+        public static List<Topic> GetTopics(string name)
+        {
+            using var context = DbServices.Instance.Context;
+            return context.Topics.Where(topic => topic.Title.Contains(name)).ToList();
+        }
+
+        public static List<Topic> GetConnectedTopics(int userId)
+        {
+            using var context = DbServices.Instance.Context;
+
+            return (from t in context.Topics 
+                    join c in context.Connects 
+                        on t.TopicsId equals c.TopicsId 
+                    where c.UserId == userId 
+                    select t
+                    ).ToList();
+        } 
+    }
+}
