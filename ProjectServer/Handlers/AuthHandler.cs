80af0e4ebdeedb0eafdf1825d2ed549c94390810
Thibault LEPEZ <thibault.lepez@efrei.net>
2020-12-15 16:08:47 +0100

    Add auth handeliing
    register still need fix

---
+++ ProjectServer/Handlers/AuthHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using ProjectServer.Models;
+using ProjectServer.Services;
+using Serilog;
+using SharedProject.CommandUtils;
+using SharedProject;
+
+namespace ProjectServer.Handlers
+{
+    public partial class WebSocketHandler    
+    {
+        private void HandleLogin (LoginData data)
+        {
+            var dbUser = AuthService.LogUserIn(data.Username, data.Password);
+
+            if (dbUser == null)
+            {
+                _response.Status = "error"; // TODO: change status here
+                _response.Data = "Wrong username or password";
+                // TODO: send response here
+                return;
+            }
+            
+            // Set the state of the user
+            _user = dbUser;
+
+            // Add the user to the Dictionary
+            _connectedClient.TryAdd(_user, _webSocket); //TODO: handle error
+            Log.Information($"{_user.Username} logged in");
+            _response.SetSuccessNoData();
+            Communication.SendResponse(_webSocket, _response);
+        }
+
+        private void HandleRegister(LoginData data)
+        {
+            var newUser = new User
+            {
+                Username = data.Username,
+                Password = AuthService.HashPassword(data.Password)
+            };
+
+            try
+            {
+                AuthService.RegisterUser(newUser);
+                Log.Information($"{_user.Username} registered");
+                _response.SetSuccessNoData();
+                Communication.SendResponse(_webSocket, _response);
+            }
+            catch
+            {
+                _response.Status = "error"; // TODO: change status here
+                _response.Data = "Error while registering user";
+                Log.Error("A registration failed");
+                Communication.SendResponse(_webSocket, _response);
+            }
+        }
+    }
+}
---
+++ ProjectServer/Services/AuthService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using ProjectServer.Models;
+
+namespace ProjectServer.Services
+{
+    public class AuthService
+    {
+        public static User LogUserIn(string username, string password)
+        {
+            var dbContext = DbServices.Instance.Context;
+            var dbUser = dbContext.Users.FirstOrDefault(user => user.Username == username);
+
+            if (dbUser == null) return null;
+
+            return BCrypt.Net.BCrypt.Verify(password, dbUser.Password) ? dbUser : null;
+        }
+
+        public static User RegisterUser(User newUser)
+        {
+            var dbContext = DbServices.Instance.Context;
+
+            try
+            {
+                dbContext.Users.Add(newUser); // TODO: handle error
+                dbContext.SaveChanges();
+                return newUser;
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+    }
+}
