2eff205583811353e7583dc227896eabf9352ca4
Thibault LEPEZ <thibault.lepez@efrei.net>
2020-12-15 16:10:37 +0100

    Add Exception for bad request

--- SharedProject/Exceptions/BadRequestException.cs
+++ SharedProject/Exceptions/BadRequestException.cs
@@ -5,5 +5,7 @@ namespace SharedProject.Exceptions
     public class BadRequestException : Exception
     {
         public BadRequestException(): base("Request do not match the required syntax ") {}
+        
+        public BadRequestException(string message): base("Bad request: " + message) {}
     }
 }
