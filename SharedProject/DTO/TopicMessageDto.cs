7a3cb1af742d900708767e016913b5a6242ff2f3
Thibault LEPEZ <thibault.lepez@efrei.net>
2021-01-10 17:52:29 +0100

    Update Topic message DTO

--- SharedProject/DTO/TopicMessageDto.cs
+++ SharedProject/DTO/TopicMessageDto.cs
@@ -1,7 +1,12 @@
-﻿namespace SharedProject.DTO
+﻿using System;
+
+namespace SharedProject.DTO
 {
     public class TopicMessageDto: CommandDto
     {
-        
+        public string TopicTitle { get; set; }
+        public string Text { get; set; }
+        public string Sender { get; set; }
+        public string CreatedAt { get; set; }
     }
 }
